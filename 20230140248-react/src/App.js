15bfb0824d15647369b368e36cc203ab1350692a
syawalqi <galihmaulanasyawalqi@gmail.com>
2025-10-02 07:23:14 +0700

    first commit

---
+++ 20230140248-react/src/App.js
@@ -0,0 +1,40 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { useEffect, useState } from 'react';
+
+function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch('http://localhost:5000')
+      .then(response => response.json())
+      .then(data => setMessage(data.message))
+      .catch(error => console.error('Error:', error));
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <h1>Selamat Datang di Aplikasi React</h1>
+          <p>Ini adalah komponen React sederhana.</p>
+          <h1>Integrasi React dan Node.js</h1>
+          <p>Pesan dari server: {message}</p>
+
+
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
