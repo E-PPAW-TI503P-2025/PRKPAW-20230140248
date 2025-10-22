2792dd0af154ad5bd9109a552aef0b0bea4b0cbf
syawalqi <galihmaulanasyawalqi@gmail.com>
2025-10-22 15:30:35 +0700

    Tugas 3

--- 20230140248-node-server/server.js
+++ 20230140248-node-server/server.js
@@ -1,14 +1,28 @@
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const port = 5000;
-const cors = require('cors');
+const PORT = 3001;
+const morgan = require("morgan");
 
+// Impor router
+const presensiRoutes = require("./routes/presensi");
+const reportRoutes = require("./routes/reports");
+
+// Middleware
 app.use(cors());
-
-app.get('/', (req, res) => {
-  res.json({ message: 'Hello from Node.js Server!' });
+app.use(express.json());
+app.use(morgan("dev"));
+app.use((req, res, next) => {
+  console.log(`${new Date().toISOString()} - ${req.method} ${req.url}`);
+  next();
 });
-
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+app.get("/", (req, res) => {
+  res.send("Home Page for API");
+});
+const ruteBuku = require("./routes/books");
+app.use("/api/books", ruteBuku);
+app.use("/api/presensi", presensiRoutes);
+app.use("/api/reports", reportRoutes);
+app.listen(PORT, () => {
+  console.log(`Express server running at http://localhost:${PORT}/`);
 });
